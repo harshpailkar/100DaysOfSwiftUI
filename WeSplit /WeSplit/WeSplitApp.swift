006a267b3746d0468e114626a20718b8e543f23c
Harsh Pailkar <85762743+harshpailkar@users.noreply.github.com>
2022-10-19 03:28:53 +0530

    Create WeSplit

---
+++ WeSplit /WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Harsh Pailkar on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
